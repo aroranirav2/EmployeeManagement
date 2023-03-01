418bda6caa0d42022e0852729ccbab5506e5fcb5
Arora Nirav <aroranirav2@outlook.com>
2023-03-01 10:19:57 -0600

    updated deprecated modules

--- EmployeeManagement_UI/src/app/employees/store/employee.effects.ts
+++ EmployeeManagement_UI/src/app/employees/store/employee.effects.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, mergeMap, of } from 'rxjs';
+import { NotificationService } from 'src/app/shared/services/notification.service';
 import { Employee } from '../models/employee.model';
 import { EmployeeService } from '../services/employee.service';
 import * as fromEmployeeAction from '../store/employee.actions';
@@ -14,12 +15,14 @@ export class EmployeeEffects {
       ofType(fromEmployeeAction.loadEmployees),
       mergeMap(() => this.employeeService.getAllEmployees().pipe(
         map((employees: Employee[]) => fromEmployeeAction.loadEmployeesSuccess({ employees })),
-        catchError((error: any) =>
-          of(fromEmployeeAction.loadEmployeesFailure({ error })))
+        catchError((error: any) => {
+          this.notificationService.showError('Error loading departments');
+          return of(fromEmployeeAction.loadEmployeesFailure({ error }));
+        }
       ))
     )
-  );
+  ));
 
-  constructor(private actions$: Actions, private employeeService: EmployeeService) {}
+  constructor(private actions$: Actions, private employeeService: EmployeeService, private notificationService: NotificationService) {}
 
 }
